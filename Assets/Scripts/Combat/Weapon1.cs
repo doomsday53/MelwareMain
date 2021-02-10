f669332fa1a126488ad0afe5e63d3970909b9ec2
shaneWilsonDestroy <shawil1.lms@gmail.com>
2021-02-09 19:35:31 -0600

    Added a charge shot, and a weapon cycle system.

---
+++ Assets/Scripts/Combat/Weapon1.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb6b9ba4e746a733dcb87d0295252bfe0bfb7962e4480826a5e4b28c6a880879
+size 4069
---
+++ Assets/Scripts/Combat/chargeShot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f555a4dffeef9e6e14a834e30fb78fad1f2d2e78875a59d456cad990a7d98265
+size 1587

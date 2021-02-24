7ab7af76d11ad2fe7b7aa7a33437d0d4a53b4bee
Nate Nicholson <nchlsnnate@gmail.com>
2021-02-23 22:25:03 -0600

    Adding what Brandon changed about the Player script to coincide with what I changed.

--- Assets/Scripts/Player/PlayerAllinOne.cs
+++ Assets/Scripts/Player/PlayerAllinOne.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50d0eafaf9eccd7b2751799fad3e1af11e666894f0175f225775deecba9b9e51
-size 10934
+oid sha256:889438951cb7aded08fd6aa36ae04bd042aee5f4eef9da2414994a8d1f0ebc30
+size 10944
